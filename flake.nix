4d8d06c6b73de9567a9a849def328696b7acab1c
Actoriu <51398008+Actoriu@users.noreply.github.com>
2021-05-22 17:18:46 +0800

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,36 @@
+description = "nix-on-droid configuration";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay";
+    };                                                  
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";              inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    NUR = {
+      url = "github:nix-community/NUR";
+      flake = false;
+    };
+
+    nix-on-droid = {
+      url = "github:t184256/nix-on-droid/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, nix-on-droid,
+ ... }: {
+    nix-on-droid = (nix-on-droid.lib.aarch64-linux.nix-on-droid {
+      config = ./nix-on-droid.nix;
+    }).activationPackage;                                 };
+}
