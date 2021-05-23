e43aea8987f9a6223bf2196dbbd258f4674b49e5
Actoriu <51398008+Actoriu@users.noreply.github.com>
2021-05-23 17:05:07 +0800

    Update fzf.nix

--- modules/programs/fzf.nix
+++ modules/programs/fzf.nix
@@ -4,8 +4,8 @@
   programs = {
     fzf = {
       enable = true;
-      enableBashIntegration = true;
-      enableZshIntegration = true;
+      # enableBashIntegration = true;
+      # enableZshIntegration = true;
       defaultOptions = [
         "--color=bg+:24"
       ];
