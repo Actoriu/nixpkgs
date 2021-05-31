23934e65d68ef681bb62c31f97b4beae0da5bd9c
Actoriu <51398008+Actoriu@users.noreply.github.com>
2021-05-31 17:09:34 +0800

    Update home.nix

--- modules/programs/home.nix
+++ modules/programs/home.nix
@@ -6,7 +6,6 @@
       nixConf = {
         target = ".config/nix/nix.conf";
         text = ''
-          auto-optimise-store = true
           keep-outputs = true
           keep-derivations = true
           substituters = https://mirror.sjtu.edu.cn/nix-channels/store https://cache.nixos.org
