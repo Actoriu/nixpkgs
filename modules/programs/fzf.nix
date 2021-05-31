717ccfcbc93c7bbb0b353e84fae71723fe6db668
Actoriu <51398008+Actoriu@users.noreply.github.com>
2021-05-31 17:08:41 +0800

    Update fzf.nix

--- modules/programs/fzf.nix
+++ modules/programs/fzf.nix
@@ -4,11 +4,6 @@
   programs = {
     fzf = {
       enable = true;
-      # enableBashIntegration = true;
-      # enableZshIntegration = true;
-      defaultOptions = [
-        "--color=bg+:24"
-      ];
     };
   };
 }
