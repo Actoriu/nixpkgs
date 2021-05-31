285bc673026244eeada9610119c961314b8bf703
Actoriu <51398008+Actoriu@users.noreply.github.com>
2021-05-31 16:45:31 +0800

    Update direnv.nix

--- modules/programs/direnv.nix
+++ modules/programs/direnv.nix
@@ -4,8 +4,6 @@
   programs = {
     direnv = {
       enable = true;
-      # enableBashIntegration = true;
-      # enableZshIntegration = true;
       enableNixDirenvIntegration = true;
     };
   };
