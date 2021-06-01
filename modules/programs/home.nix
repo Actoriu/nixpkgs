a22d274c249461d0fb927c2535c214b0f3de2de1
Actoriu <51398008+Actoriu@users.noreply.github.com>
2021-06-01 20:45:41 +0800

    Update home.nix

--- modules/programs/home.nix
+++ modules/programs/home.nix
@@ -6,6 +6,7 @@
       nixConf = {
         target = ".config/nix/nix.conf";
         text = ''
+          experimental-features = nix-command flakes
           keep-outputs = true
           keep-derivations = true
           substituters = https://mirror.sjtu.edu.cn/nix-channels/store https://cache.nixos.org
@@ -16,7 +17,7 @@
   };
 
   # Use the same overlays as the system packages
-  #nixpkgs = {
-  #  overlays = config.nixpkgs.overlays;
-  #};
+  # nixpkgs = {
+  #   overlays = config.nixpkgs.overlays;
+  # };
 }
