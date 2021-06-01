86e1fa3a6b17a2fd7598da3cf64238471126cf5f
Actoriu <51398008+Actoriu@users.noreply.github.com>
2021-06-01 22:24:05 +0800

    Update fzf.nix

--- modules/programs/fzf.nix
+++ modules/programs/fzf.nix
@@ -7,9 +7,7 @@
       tmux = {
         enableShellIntegration = true;
       };
-      defaultCommand = [
-        "rg --files --no-ignore --hidden --follow -g '!{.git,target,node_modules,*.swp,dist,*.coffee}/*' 2> /dev/null"
-      ];
+      defaultCommand = "rg --files --no-ignore --hidden --follow -g '!{.git,target,node_modules,*.swp,dist,*.coffee}/*' 2> /dev/null";
     };
   };
 }
