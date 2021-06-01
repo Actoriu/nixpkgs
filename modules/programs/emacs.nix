dcdee5da563e42d589a79ecf4b6eae17913d63fc
Actoriu <51398008+Actoriu@users.noreply.github.com>
2021-06-01 21:03:50 +0800

    Update emacs.nix

--- modules/programs/emacs.nix
+++ modules/programs/emacs.nix
@@ -1,42 +1,57 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  #nixpkgs= {
-  #  overlays = [
-  #    (import (builtins.fetchTarball {
-  #      url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
-  #    }))
-  #  ];
-  #};
-
   programs = {
     emacs = {
       enable = true;
-      #package = pkgs.emacsPgtkGcc;
-      #extraPackages = epkgs: with epkgs; [
-      #  evil
-      #  helm
-      #  general
-      #  magit
-      #  nix-mode
-      #  company
-      #];
+      package = if pkgs.stdenv.isDarwin then pkgs.emacsMacport else pkgs.emacsPgtkGcc;
+      # extraPackages = epkgs: with epkgs; [
+      #   evil
+      #   helm
+      #   general
+      #   magit
+      #   nix-mode
+      #   company
+      # ];
     };
   };
 
   home = {
     packages = with pkgs; [
-      # ccls
+      ccls
       ripgrep
-      # rnix-lsp
       guile
-      # nodejs
       openssh
-      python38
-      python38Packages.ipython
-      python38Packages.pip
-      python38Packages.epc
+      python39
+      python39Packages.ipython
+      python39Packages.pip
+      python39Packages.epc
     ];
   };
+
+  home = {
+    file = {
+      ".emacs.d" = {
+        source = builtins.fetchGit {
+          url = "https://github.com/syl20bnr/spacemacs";
+          ref = "develop";
+        };
+      };
+    };
+    recursive = true;
+  };
+
+  # home = {
+  #   file = {
+  #     ".spacemacs.d" = {
+  #       # source = ./init.el;
+  #       source = builtins.fetchGit {
+  #         url = "https://github.com/Actoriu/spacemacs.d";
+  #         ref = "master";
+  #       };
+  #       recursive = true;
+  #     };
+  #   };
+  # };
 
 }
