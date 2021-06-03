05a312d5a29e1b33965af95df50aef5cb09cbfac
Actoriu <51398008+Actoriu@users.noreply.github.com>
2021-06-03 20:33:53 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,8 +2,18 @@
   description = "nix-on-droid configuration";
 
   inputs = {
+  
     nixpkgs = {
+      url = "github:nixos/nixpkgs/master";
+    };
+    
+    nixpkgs-unstable = {
       url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+
+    nix = {
+      url = "github:nixos/nix";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     emacs-overlay = {
@@ -17,7 +27,7 @@
 
     # home-manager = {
     #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
+    #   inputs.nixpkgs.follows = "nixpkgs-unstable";
     # };
 
     nur = {
@@ -27,11 +37,11 @@
 
     nix-on-droid = {
       url = "github:t184256/nix-on-droid/master";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
   };
 
-  outputs = { self, nixpkgs, nix-on-droid, ... }: {
+  outputs = { self, nixpkgs-unstable, nix, nix-on-droid, ... }: {
     nix-on-droid = (nix-on-droid.lib.aarch64-linux.nix-on-droid {
       config = ./nix-on-droid.nix;
     }).activationPackage;
