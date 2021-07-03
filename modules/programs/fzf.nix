c100f03f579b8d76bfc9da83d0338a8c0eb84408
Actoriu <51398008+Actoriu@users.noreply.github.com>
2021-07-03 19:46:47 +0800

    Update fzf.nix

--- modules/programs/fzf.nix
+++ modules/programs/fzf.nix
@@ -4,7 +4,7 @@
   programs = {
     fzf = {
       enable = true;
-      defaultCommand = "${pkgs.ripgrep}/bin/rg --files --hidden --follow --no-ignore -g '!{.git,target,node_modules,*.swp,dist,*.coffee}/*' 2> /dev/null";
+      # defaultCommand = "${pkgs.ripgrep}/bin/rg --files --hidden --follow --no-ignore -g '!{.git,target,node_modules,*.swp,dist,*.coffee}/*' 2> /dev/null";
     };
   };
 }
